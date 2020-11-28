a53cf2a602df56e1f97a9ccb5e8d7a9fc5a0ed5a
ybgirgin3 <ybgirgin3@gmail.com>
2020-11-28 22:15:47 +0300

    asd

--- interactions_/version2/inf.py
+++ interactions_/version2/inf.py
@@ -1,6 +1,5 @@
-username = "antispeedbump_official"
-password = "yusufberkaygirgin2580"
-"""
+#username = "antispeedbump_official"
+#password = "yusufberkaygirgin2580"
+
 username = 'mental_huzur'
 password = 'manzaralar1234'
-"""
