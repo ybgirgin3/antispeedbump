663a30a2df63b4e2510c55979264e36829d7be17
ybgirgin3 <ybgirgin3@gmail.com>
2020-12-09 00:47:45 +0300

    video upload işi tam olarak entegre edildi fakat henüz test edilmedi

--- uploading/post_/photo_upload.py
+++ uploading/post_/photo_upload.py
@@ -1,52 +1,14 @@
-import os 
+import os
 import shutil
 from instabot import Bot
 from instabot.api.api_photo import compatible_aspect_ratio, get_image_size
 import random
 import time
 from process_image import prep_and_fix
-#from notify_run import Notify
-#notif = Notify()
-
-
-# tag dictleri
-M =  {
-    "regular": "#bmw #car #luxury #sports #carsofinstagram #m4 #m3 #m2 #1m #7 #m6 #m8",
-    "eski_model": "#bmw #bmw2002 #classical #nostalgia #old #oldcar #2002 #carsofinstagram",
-    "7_series": "#bmw #m #7 #The7 #7Series #7series #bmw7 #v12 #v8 #luxury",
-    "M8": "#bmw #bmwm8 #m8 #m8competition #v8 #car #carsofinstagram",
-    "M6": "#bmw #bmwm6 #m6 #v8 #m6grandcoupe #m6coupe #car #carsofinstagram #twinturbo",
-    "M5": "#bmw #bmwm5 #m5 #v8 #m5competition #car #carsofinstagram #twinturbo #m5lci",
-    "M4": "#bmw #bmwm4 #m4 #S55 #convertible #m4competition #car #carsofinstagram #twinturbo #m4csl",
-    "M3": "#bmw #bmwm3 #m3 #S55 #v8 #e92 #f82 #g20 #m3competition #twinturbo #e46 #m3csl",
-    "M2": "#bmw #bmwm2 #m2 #S55 #f87 #turbocharged #twinturbo #m2csl #m3competition #m2csl"
-}
-
-
-X =  {
-    "X3": "#bmw #bmwx3 #x3 #x3m #x3mcompetition #S55 #car #carsofinstagram #turbocharged",
-    "X5": "#bmw #bmwx5 #x5 #x5m #x5mcompetition #v8 #twinturbo #turbocharged #suv #carsofinstagram",
-    "X6": "#bmw #bmwx6 #x6 #x6m #x6mcompetition #v8 #twinturbo #turbocharged #suv #twinturbocharged",
-    "X7": "#bmw #bmwx7 #x7 #x7m50i #x7mcompetition #v8 #twinturbo #turbocharged #suv #twinturbocharged"
-}
+from json_funcker import json_funcker
 
 username = 'antispeedbump_bmw_official'
 password = 'yusufberkaygirgin2580'
-
-def json_funcker(model):
-    sp = model.split(",")
-    tag = None
-    if sp[1] == 'M':
-        if sp[2] in M:
-            tag =  M[sp[2]]
-
-    elif sp[1] == 'X':
-        if sp[2] in X:
-            tag = X[sp[2]]
-
-    # ret = "M", "M6", "white", "#bmwm6"
-    ret = sp[1], sp[2], sp[3], tag
-    return ret
 
 
 # https://github.com/basnijholt/instacron/blob/master/instacron.py#L378
@@ -73,10 +35,9 @@ def job():
     caption = os.path.splitext(imagename)[0]
     tags = full_im[3]
     upload = bot.upload_photo(pic, caption = """
-            {0}
-
-
+{0}
 Follow for more 👉🏻 @{1}
+
 💪🏻Don't forget to like save and share with your friends
 💪🏻Don't forget to turn on notifications
 ✉ Dm for credit
@@ -95,7 +56,6 @@ Follow for more 👉🏻 @{1}
         #notif.send('{} isimli post başarılı bir şekilde instagramda paylaşıldı.'.format(image))
         #os.system(f'mv {full_image_path} {sent_path}')
         shutil.move('{}'.format(full_image_path), sent_path)
-        shutil.rmtree('/config', ignore_errors=True)
         #sys.exit(0)
     else:
         #print('gönderilemedi.. yeniden başlıyor.')
---
+++ uploading/post_/video_upload.py
@@ -0,0 +1,75 @@
+"""
+param1  :   path to video file
+param2  :   caption
+param3  :   thumbnail
+"""
+
+### NOTE:
+## instagramdaki pixel sınırlamasından dolayı sadece yatay ekran boyutundaki videolar gönderilebiliyor
+## youtube videolarından kestiğim videolarda şimdilik bir sıkıntı gözükmüyor
+
+from instabot.api.api_video import get_video_info, configure_video, resize_video
+from instabot import Bot
+
+from thumbnailer import thumbnailed
+import sys
+from json_funcker import json_funcker
+
+
+"""
+usern = 'mental_huzur'
+passw = 'manzaralar2580'
+"""
+username = 'antispeedbump_bmw_official'
+password = 'yusufberkaygirgin2580'
+
+
+def video_job():
+    # login
+    bot = Bot()
+    bot.login(username = username, password = password)
+
+    # videoyu direk olarak tanımlamaktansa fotolardaki gibi
+    # verdiğim pathten bulacak
+    path = '/media/berkay/Elements/editlenecek_videolar/hazır_videolar/'
+
+    # choose video
+    video = sorted([video for video in os.listdir(path) if os.path.splitext(video)[1] == '.mp4'])
+
+    # show chosen video
+    choosen_vid_path = os.path.join(path, video[0])
+    print(f'choosen video: {choosen_vid_path}')
+
+    # caption
+    full_vid = json_funcker(video[0])
+    videoname = full_vid[2]
+    caption = os.path.splitext(videoname)[0]
+
+    # tags
+    tags = full_vid[3]
+
+    # thumbnail
+    thumb = thumbnailed(video)
+    video = resize_video(video)
+    print(f'choosen vid: {video}')
+    upload_vid = bot.upload_video(video, caption= """
+{0}
+Follow for more 👉🏻 @{1}
+
+💪🏻Don't forget to like save and share with your friends
+💪🏻Don't forget to turn on notifications
+✉ Dm for credit
+
+
+{2}
+
+    """.format(caption, username, tags), thumbnail=thumb)
+
+    if upload_vid:
+        print('oldu amk')
+    else:
+        print('olmadı yine aq')
+
+
+
+video_job()
