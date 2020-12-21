830f02442b193d67492ce7c9cfa07ca394cdff25
ybgirgin3 <ybgirgin3@gmail.com>
2020-12-21 17:39:34 +0300

    path'ler düzeltildi

--- uploading/post_/video_uploader.sh
+++ uploading/post_/video_uploader.sh
@@ -1,1 +1,1 @@
-/home/berkay/miniconda3/envs/antispeed/bin/python video_upload.py
+/home/berkay/miniconda3/envs/antispeed/bin/python /home/berkay/code/SocialMedia/antispeedbump/uploading/post_/video_upload.py
