068d8d4e161e3aad786a688371f6de06fa198d51
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-12-18 00:13:28 +0300

    bot.py is now center of the whole project

--- bot.py
+++ bot.py
@@ -1,6 +1,21 @@
-from utils import Fetch
-from utils import headers
+from utils import FileProcess, Fetch
+import sys
 
-ret = Fetch(username="kimkardashian").fetch()
-print(ret)
+# get data
+username = sys.argv[1]
 
+# if file exists
+content = None
+is_exists = FileProcess(filename=username).is_site_exists()
+print("is_exists", is_exists)
+if isinstance(is_exists, str):
+    content = is_exists
+elif not is_exists:
+    # fetch data
+    resp = Fetch(username=username).fetch()
+    saved_resp = FileProcess(filename=username, content=resp).write()
+    content = FileProcess(filename=username).read()
+
+    print("content:", content[0])
+
+# extract content
