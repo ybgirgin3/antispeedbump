3ac0d4a1c2974d1cc63fc923feeaaf5c695e6506
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-12-18 16:09:11 +0300

    new type of sites file handling implemented

--- utils/fp.py
+++ utils/fp.py
@@ -6,34 +6,23 @@ import os
 
 class FileProcess:
     def __init__(self, filename: str,
-                 # content: Optional[Union[str, dict]],
-                 dtype: str = "json",
                  *args, **kwargs
                  ) -> None:
-        self.filepath = os.path.join('sites', f'{filename}.json')
+        self.filepath = os.path.join(
+            kwargs.get("root", "sites"), f'{filename}.json')
         self.content = kwargs.get('content', {})
-        self.dtype = dtype
 
     def read(self):
-        "read json and yaml file"
-        ret = None
-        if self.dtype == "yml":
-            ret = yaml.safe_load(open(self.filepath))
-        else:
-            with open(self.filepath, "r") as f:
-                ret = json.load(f)
+        "read json"
+        with open(self.filepath, "r") as f:
+            ret = json.load(f)
 
         return ret
 
     def write(self):
         "write json files"
         print("filename in write")
-        c2r = ""
-        if self.dtype == "json":
-            c2r = json.dumps(self.content, indent=2)
-        else:
-            c2r = self.content
-
+        c2r = json.dumps(self.content, indent=2)
         with open(self.filepath, "w") as f:
             f.write(c2r)
 
