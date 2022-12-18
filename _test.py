172d0dc8a5dccb2e555fd9749547be3238133f01
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-12-19 01:15:49 +0300

    pprint

--- _test.py
+++ _test.py
@@ -1,7 +1,12 @@
 from utils import Parse, FileProcess
+from utils import Expressions
+from pprint import pprint
 
 
 
 username = 'bellathorne'
 content = FileProcess(filename=username).read()
 ret = Parse(content=content).find_val()
+pprint(ret)
+
+
