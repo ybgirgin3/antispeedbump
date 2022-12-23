e80be499e8b656a556c83253e31ab821504a3f6e
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-12-23 21:18:56 +0300

    bot is now class

--- bot.py
+++ bot.py
@@ -1,32 +1,42 @@
 from utils import FileProcess, Process
+from utils import Post
 from typing import Optional
-import sys
 
-username = sys.argv[1]
 
 
 class Bot:
-    def __init__(self, target_user: Optional[str] = None):
+    def __init__(self,
+                 target_user: Optional[str] = "",
+                 image: Optional[dict] = {}
+                 ):
         self.target_user = target_user
+        self.image = image
 
     def get_data_from_another(self):
         # if file exists
         content = None
-        is_site_exist = FileProcess(filename=username).is_site_exists()
+        is_site_exist = FileProcess(filename=self.target_user).is_site_exists()
         if not is_site_exist:
             "if username is new create user data"
-            print("data not exists")
-            resp: dict = Process(username=username).fetch()
-            FileProcess(filename=username, content=resp).write()
+            resp: dict = Process(username=self.target_user).fetch()
+            FileProcess(filename=self.target_user, content=resp).write()
 
         # extract content
-        content = FileProcess(filename=username).read()
+        content = FileProcess(filename=self.target_user).read()
         extracted = Process(content=content).parse()
         return extracted
 
     def post_content(self):
-        pass
+        ret = Post(image=self.image).process()
+        return ret
 
 
-user_content = Bot(target_user=username).get_data_from_another()
-print("user_content: ", user_content)
+# username = sys.argv[1]
+# user_content = Bot(target_user=username).get_data_from_another()
+# print(user_content)
+# image = {
+#     "path": "images/soldier.jpeg",
+#     "description": "a cat which is ready to defend country"
+#     }
+# post_content = Bot(image=image).post_content()
+# print("post_content: ", post_content)
