fce777ecf333cb36b992251b9dd694408f9a8e74
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-12-23 21:18:45 +0300

    test updated

--- _test.py
+++ _test.py
@@ -1,11 +1,24 @@
-from utils import Process, FileProcess
-from pprint import pprint
+# from utils import Process, FileProcess
+# from pprint import pprint
+# import os
+from utils import Post
+
+
+p = Post()
+print("post: ", p)
+
+
+# env = True if 'DEBUG' in os.environ else False
+# print(env)
 
 
 
-username = 'bellathorne'
-content = FileProcess(filename=username).read()
-ret = Process.Parse(content=content).find_val()
-pprint(ret)
+# ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# print(ROOT_DIR)
+
+#username = 'bellathorne'
+#content = FileProcess(filename=username).read()
+#ret = Process.Parse(content=content).find_val()
+#pprint(ret)
 
 
