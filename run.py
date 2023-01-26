c84d4a42e9ac2e23e76e830ec66453b12ad8071b
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-01-26 21:16:17 +0300

    bot table creation is done automatically

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
-from pprint import pprint
 from pathlib import Path
-import os
+from pprint import pprint
 
 import click
 
 from bot import Bot
-
-
-
 
 
 @click.group()
@@ -21,13 +17,14 @@ def cli():
 def find(username: str, collect: bool):
     if not Path("antispeedbump.db").exists():
         from commons import SQL_ALCHEMY_ENGINES, _create_table
-        _create_table("Sites", SQL_ALCHEMY_ENGINES['sites'])
-
+        _create_table("Sites", SQL_ALCHEMY_ENGINES['antispeedbump'])
+        _create_table("Queue", SQL_ALCHEMY_ENGINES['antispeedbump'])
 
     ret = Bot(target_user=username,
               will_create_content=collect,
               ).get_data_from_another()
-    print(ret['full_name'], ret['profile_picture'])
+    # print(ret['full_name'], ret['profile_picture'])
+    print(ret)
 
 
 @cli.command()
